12f6926b2323cc5be267f70308918c6ea21dc73b
zheng-oh <894389673@qq.com>
2021-01-09 23:50:25 +0800

    字体20

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=30:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
